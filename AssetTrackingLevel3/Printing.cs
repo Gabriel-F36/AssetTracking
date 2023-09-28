7138ceb201e75c5b5a581a65b907a02366125beb
Gabriel-F36 <gabriel.emil.johansson@gmail.com>
2023-09-28 11:19:28 +0200

    This is level 3. Since I accidentely did level 3 in the level 2 folder this is just a copy of level 2 code

---
+++ AssetTrackingLevel3/Printing.cs
@@ -0,0 +1,90 @@
+﻿using Asset_tracking_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTrackinglevel2
+{
+    internal class Printing
+    {
+        // conversion rates (2023-09-28)
+        private double USD = 1;
+        private double SEK = 11.04;
+        private double EUR = 0.95;
+        private double localPrice;
+
+        private DateTime currentDate = DateTime.Now;
+        
+        // Prints a list in a nice table format
+        public void PrintTableOfContent(List<Product> productList)
+        {
+            Console.WriteLine(
+                "Type".PadRight(15) + 
+                "Brand".PadRight(15) + 
+                "Model".PadRight(15) + 
+                "Office".PadRight(15) + 
+                "Purchase Date".PadRight(15) + 
+                "Price (USD)".PadRight(15) + 
+                "Currency".PadRight(15) + 
+                "Local price");
+
+            Console.WriteLine(
+                "----".PadRight(15) + 
+                "-----".PadRight(15) + 
+                "-----".PadRight(15) + 
+                "------".PadRight(15) + 
+                "-------------".PadRight(15) + 
+                "-----------".PadRight(15) + 
+                "--------".PadRight(15) + 
+                "-----------");
+
+            foreach (Product p in productList)
+            {
+                if ((365*3) - CheckEndOfLife(p.PurchaseDate) < 180)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+                if ((365 * 3) - CheckEndOfLife(p.PurchaseDate) < 90)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                }
+
+                if (p.Currency.ToLower().Trim() == "eur")
+                {
+                    localPrice = p.Price * EUR;
+                }
+                else if (p.Currency.ToLower().Trim() == "sek")
+                {
+                    localPrice = p.Price * SEK;
+                }
+                else if (p.Currency.ToLower().Trim() == "usd")
+                {
+                    localPrice = p.Price * USD;
+                }
+                
+                Console.Write(
+                       p.GetType().Name.PadRight(15) +
+                       p.Brand.PadRight(15) +
+                       p.Model.PadRight(15) +
+                       p.Office.PadRight(15) +
+                       p.PurchaseDate.PadRight(15) +
+                       p.Price.ToString().PadRight(15) +
+                       p.Currency.PadRight(15) +
+                       localPrice.ToString("#.##"));
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+        // Checking how old a product is
+        private int CheckEndOfLife(string date)
+        {
+            DateTime Date = Convert.ToDateTime(date);
+            TimeSpan timeSpan = currentDate - Date;
+            int daysBetween = timeSpan.Days;
+            //daysBetween = (365 * 3) - daysBetween;
+            return daysBetween;
+        }
+    }
+}
---
+++ AssetTrackingLevel3/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+
+using Asset_tracking_2;
+using AssetTrackinglevel2;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+List<Product> productList = new List<Product>();
+AppMessages appMessages = new AppMessages();
+AddProduct addProduct = new AddProduct();
+
+
+// creating a set list of products, mostly for testing purposes
+
+productList.Add(new Computer("HP", "Elitebook", "Sweden", "2020-10-02", "SEK", 588));
+productList.Add(new Computer("Asus", "W234", "USA", "2017-04-21", "USD", 1200));
+productList.Add(new Computer("Lenovo", "Yoga 730", "USA", "2018-05-28", "USD", 835));
+productList.Add(new Computer("Lenovo", "Yoga 730", "USA", "2019-05-21", "USD", 1030));
+productList.Add(new Computer("HP", "Elitebook", "Spain", "2019-06-01", "EUR", 1423));
+
+productList.Add(new Phone("iPhone", "8", "Spain", "2018-12-29", "EUR", 970));
+productList.Add(new Phone("iPhone", "11", "Spain", "2020-09-25", "EUR", 990));
+productList.Add(new Phone("iPhone", "X", "Sweden", "2018-07-15", "SEK", 1245));
+productList.Add(new Phone("Motorola", "Razr", "Sweden", "2020-03-16", "SEK", 970));
+
+string typeName;
+
+while (true)
+{
+    appMessages.Instructions();
+    Console.Write("Type of product: ");
+    typeName = Console.ReadLine();
+    if (typeName.ToLower().Trim() == "q")
+    {
+        break;
+    }
+    if (typeName.ToLower().Trim() == "p")
+    {
+        addProduct.Phone(productList);
+    }
+    else if (typeName.ToLower().Trim() == "c")
+    {
+        addProduct.Computer(productList);
+    }
+    appMessages.SuccessfulAdd();
+}
+
+//List<Product> productListOrder = productList.OrderBy(x => x.GetType().Name).ToList();
+List<Product> productListOrderOffice = productList.OrderBy(x => x.Office).ThenBy(y => y.PurchaseDate).ToList();
+
+// printing tableformat and all content
+
+new Printing().PrintTableOfContent(productListOrderOffice);
+
+Console.ReadLine();
