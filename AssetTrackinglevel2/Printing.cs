8405dd9a5e718531b07c1942a3d2a096c4b48d6c
Gabriel-F36 <gabriel.emil.johansson@gmail.com>
2023-09-28 11:15:39 +0200

    Added converter information in printing class new errorhandling for currency input

--- AssetTrackinglevel2/Printing.cs
+++ AssetTrackinglevel2/Printing.cs
@@ -9,6 +9,12 @@ namespace AssetTrackinglevel2
 {
     internal class Printing
     {
+        // conversion rates (2023-09-28)
+        private double USD = 1;
+        private double SEK = 11.04;
+        private double EUR = 0.95;
+        private double localPrice;
+
         private DateTime currentDate = DateTime.Now;
         
         // Prints a list in a nice table format
@@ -36,32 +42,48 @@ namespace AssetTrackinglevel2
 
             foreach (Product p in productList)
             {
-                if (CheckEndOfLife(p.PurchaseDate) < 180)
+                if ((365*3) - CheckEndOfLife(p.PurchaseDate) < 180)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                 }
-                if (CheckEndOfLife(p.PurchaseDate) < 90)
+                if ((365 * 3) - CheckEndOfLife(p.PurchaseDate) < 90)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                 }
+
+                if (p.Currency.ToLower().Trim() == "eur")
+                {
+                    localPrice = p.Price * EUR;
+                }
+                else if (p.Currency.ToLower().Trim() == "sek")
+                {
+                    localPrice = p.Price * SEK;
+                }
+                else if (p.Currency.ToLower().Trim() == "usd")
+                {
+                    localPrice = p.Price * USD;
+                }
+                
                 Console.Write(
-                       p.GetType().Name.PadRight(15) + " " +
-                       p.Brand.PadRight(15) + " " +
-                       p.Model.PadRight(15) + " " +
-                       p.Office.PadRight(15) + " " +
-                       p.PurchaseDate.PadRight(15) + " " +
-                       p.Price.ToString().PadRight(15) + " " +
-                       p.Currency.PadRight(15));
+                       p.GetType().Name.PadRight(15) +
+                       p.Brand.PadRight(15) +
+                       p.Model.PadRight(15) +
+                       p.Office.PadRight(15) +
+                       p.PurchaseDate.PadRight(15) +
+                       p.Price.ToString().PadRight(15) +
+                       p.Currency.PadRight(15) +
+                       localPrice.ToString("#.##"));
                 Console.ResetColor();
                 Console.WriteLine();
             }
         }
+        // Checking how old a product is
         private int CheckEndOfLife(string date)
         {
             DateTime Date = Convert.ToDateTime(date);
             TimeSpan timeSpan = currentDate - Date;
             int daysBetween = timeSpan.Days;
-            daysBetween = (365 * 3) - daysBetween;
+            //daysBetween = (365 * 3) - daysBetween;
             return daysBetween;
         }
     }
